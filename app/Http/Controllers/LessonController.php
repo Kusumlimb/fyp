87b177775765db6600d3b56d9850f8a7dd91f729
Kusumlimb <kusumlimbu75@gmail.com>
2025-03-08 09:27:56 +0545

    video player

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -10,6 +10,7 @@ class LessonController extends Controller
 
      public function show(Course $course, Lesson $lesson)
      {
+          $course->load(['lessons:course_id,slug,title']);
           return view('front.lesson')->with(['course' => $course, 'lesson' => $lesson]);
      }
 
--- resources/views/front/lesson.blade.php
+++ resources/views/front/lesson.blade.php
@@ -4,19 +4,23 @@
     <div class="w-1/4 bg-gray-100 p-6 shadow-xl rounded-lg flex flex-col border border-blue-400/20">
         <h2 class="text-2xl font-semibold mb-6 text-gray-800">Lessons</h2>
         <ul class="space-y-4 flex-1 overflow-y-auto">
-            <li class="p-4 bg-white rounded-lg cursor-pointer text-gray-700 hover:bg-blue-100 hover:text-blue-800 transition duration-300 font-medium shadow-sm" onclick="playVideo('video1.mp4')">Lesson 1: Introduction</li>
-            <li class="p-4 bg-white rounded-lg cursor-pointer text-gray-700 hover:bg-blue-100 hover:text-blue-800 transition duration-300 font-medium shadow-sm" onclick="playVideo('video2.mp4')">Lesson 2: Basics</li>
-            <li class="p-4 bg-white rounded-lg cursor-pointer text-gray-700 hover:bg-blue-100 hover:text-blue-800 transition duration-300 font-medium shadow-sm" onclick="playVideo('video3.mp4')">Lesson 3: Advanced Concepts</li>
+            @foreach($course->lessons as $courseLesson)
+                <li>
+                    <a class="w-full inline-block p-4  rounded-lg cursor-pointer hover:bg-blue-100 {{$courseLesson->slug === $lesson->slug ? 'text-blue-800 bg-blue-100': 'text-gray-700 bg-white'}} hover:text-blue-800 transition duration-300 font-medium shadow-sm" href="{{route('front.courses.lessons.view', ['course' => $course->slug, 'lesson' => $courseLesson->slug])}}">
+                    Lesson {{$loop->count}}: {{ucwords($courseLesson->title)}}
+                    </a>
+                </li>
+            @endforeach
         </ul>
     </div>
-    <div class="flex-1 flex flex-col items-center bg-gray-100 shadow-xl rounded-lg p-6 border border-blue-400/20">
-        <h2 class="text-2xl font-semibold text-gray-800 mb-4">Lesson Video</h2>
+    <div class="flex-1 bg-gray-100 shadow-xl rounded-lg p-6 border border-blue-400/20">
+        <h2 class="text-2xl font-semibold text-gray-800 mb-4">{{ucwords($lesson->title)}}</h2>
         <div class="w-full rounded-lg overflow-hidden shadow-lg border border-gray-200 bg-white">
             <video
-                    id="lesson-video-{{$lesson->slug}}"
-                    class="video-js vjs-fluid vjs-big-play-centered"
+                    id="lesson-video"
+                    class="video-js vjs-big-play-centered"
                     controls
-                    data-setup='{}'>
+                   >
                 <source src="{{ \Illuminate\Support\Facades\Storage::disk('public')->url($lesson->video_url) }}" />
                 <p class="vjs-no-js">
                     To view this video please enable JavaScript, and consider upgrading to a
@@ -27,6 +31,24 @@
                 </p>
             </video>
         </div>
+        <p class="text-gray-700 mt-3 text-left">
+            {!! $lesson->description !!}
+        </p>
     </div>
 </div>
 @endsection
+@push('scripts')
+    <script type="module">
+        let player = videojs('lesson-video',{
+            // autoplay: true,
+            fluid: true,
+            plugins: {
+                hotkeys: {
+                    volumeStep: 0.1,
+                    seekStep: 5,
+                    enableModifiersForNumbers: false,
+                },
+            },
+        });
+    </script>
+@endpush
