23597dfb86a65fe3ff175521030d4de99949e53f
Kusumlimb <kusumlimbu75@gmail.com>
2025-05-17 23:30:45 +0545

    textarea mod

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,17 +7,13 @@ import.meta.glob([
 import 'video.js/dist/video-js.css';
 import videojs from 'video.js';
 import "videojs-hotkeys";
-
+import autosize from 'autosize';
 import { Accordion } from 'flowbite';
+
+
 window.Alpine = Alpine;
 Alpine.start();
 window.videojs = videojs;
 
 
-$("textarea").each(function () {
-    this.style.height = this.scrollHeight + "px";
-    this.style.overflowY = "hidden";
-}).on("input", function () {
-    this.style.height = "auto";
-    this.style.height = this.scrollHeight + "px";
-});
+autosize($("textarea"))
